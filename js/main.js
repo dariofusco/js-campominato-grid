1aaa48c8c53118689ef37231ac6e8e60d53f4612
dariofusco <fdario@hotmail.it>
2023-07-11 17:33:13 +0200

    colora quadrato al click

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 document.getElementById("myBtn").addEventListener("click", function () {
+
     const div = document.querySelector("div.row")
 
     for (i = 1; i <= 100; i++) {
         const singleSquare = `<div class="${i} border border-1 square">${i}<div>`
         div.innerHTML += singleSquare
+
     }
-    div.addEventListener("click", function () {
-        div.classList.toggle("bg-success")
+    document.querySelector(".square").addEventListener("click", function () {
+        document.querySelector(".square").classList.toggle("bg-success")
     })
 })
+
+
 
 
 
