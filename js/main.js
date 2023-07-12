0efe9bfb74c90e0adfb9d3029a8d62d6b662d177
dariofusco <fdario@hotmail.it>
2023-07-12 09:49:04 +0200

    fix toggle

--- js/main.js
+++ js/main.js
@@ -2,14 +2,20 @@ document.getElementById("myBtn").addEventListener("click", function () {
 
     const div = document.querySelector("div.row")
 
-    for (i = 1; i <= 100; i++) {
-        const singleSquare = `<div class="${i} border border-1 square">${i}<div>`
+    for (let i = 1; i <= 100; i++) {
+        const singleSquare = `<div class="${i} border border-1 square">${i}</div>`
         div.innerHTML += singleSquare
 
     }
-    document.querySelector(".square").addEventListener("click", function () {
-        document.querySelector(".square").classList.toggle("bg-success")
-    })
+    const squareList = document.querySelectorAll(".square")
+
+    for (let i = 0; i < squareList.length; i++) {
+        squareList[i].addEventListener("click", function () {
+            squareList[i].classList.toggle("bg-success")
+            console.log(squareList[i].innerHTML)
+        })
+    }
+    
 })
 
 
